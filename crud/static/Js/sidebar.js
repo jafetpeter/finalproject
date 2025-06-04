d8024dafb29b9cfa19c102f8a075fce256d50cef
jafetper <jafetjosedelosreyes@gmail.com>
2025-06-04 11:06:49 +0800

    fixed the reponsiveness for now. Shift and employee for now.

--- crud/static/Js/sidebar.js
+++ crud/static/Js/sidebar.js
@@ -1,14 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const sidebar = document.getElementById('logo-sidebar');
-    const overlay = document.getElementById('sidebar-overlay');
-    const toggleButton = document.getElementById('mobile-menu-button');
-    
-    function toggleSidebar() {
+  const sidebar = document.getElementById('logo-sidebar');
+  const overlay = document.getElementById('sidebar-overlay');
+  const toggleButton = document.getElementById('mobile-menu-button');
+
+  function toggleSidebar() {
       sidebar.classList.toggle('-translate-x-full');
-      sidebar.classList.toggle('translate-x-0');
       overlay.classList.toggle('hidden');
-    }
-  
-    toggleButton.addEventListener('click', toggleSidebar);
-    overlay.addEventListener('click', toggleSidebar);
-  });
+  }
+
+  toggleButton.addEventListener('click', toggleSidebar);
+  overlay.addEventListener('click', toggleSidebar);
+});
